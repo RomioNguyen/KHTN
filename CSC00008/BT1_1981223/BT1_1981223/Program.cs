40ceca4e53f4990434093a543f077c7f8d59f964
Romio <khuong.nguyen@altsourcesoftware.com>
2022-01-31 10:12:58 +0700

    [romio] init code for BT1 CSC00008

---
+++ CSC00008/BT1_1981223/BT1_1981223/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BT1_1981223
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
